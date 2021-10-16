86f285ff0d617802ed4437b5fb1f29e62703c41a
Angelo Adduci <angelo.adduci@pallino.it>
2021-10-16 20:01:10 +0200

    Init project

---
+++ tests/Feature/Database/v1/UserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Database\v1;
+
+use App\Models\Database\v1\User;
+use Tests\TestCase;
+
+/**
+ * Class UserTest.
+ *
+ * @coversDefaultClass \App\Models\Database\v1\User
+ */
+class UserTest extends TestCase
+{
+    /**
+     * Test if User can be persisted.
+     *
+     * @test
+     * @testdox Test if User can be persisted.
+     *
+     * @return void
+     */
+    public function it_can_be_persisted(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->make();
+
+        $user->save();
+
+        $this->assertNotNull($user->id);
+    }
+}
