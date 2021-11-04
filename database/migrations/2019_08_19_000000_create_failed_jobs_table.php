ebe1fa479aa0052d3102909ca826ca6a687189c1
Angelo Adduci <angelo.adduci@pallino.it>
2021-11-04 16:52:50 +0100

    Add keyword final to migrations

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -4,7 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+/**
+ * Class CreateFailedJobsTable.
+ */
+final  class CreateFailedJobsTable extends Migration
 {
     /**
      * Run the migrations.
